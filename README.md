78a5a25fd773081fc4e9a79ef1f2a0d4cb2a4b63
Hadi Kachmar <HADIKACHMAR8@GMAIL.COM>
2021-05-21 20:17:16 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Instagram UI Redesign
+ An Instagram UI redesign in Flutter 
+ 
+## Design Inspiration
+- [Link](https://www.instagram.com/p/BvTNK7IhG-k/?utm_source=ig_web_options_share_sheet)
+
+## ScreenShots
+
+<br>
+
+<img height=600 width=300 src="https://github.com/usman18/Flutter-UI-Kit/blob/master/%2311_instagram_ui_redesign/Screenshots/insta1.JPG" hspace=80/><img height=600 width=300 src="https://github.com/usman18/Flutter-UI-Kit/blob/master/%2311_instagram_ui_redesign/Screenshots/insta2.JPG"/></br></br>
+ 
+ 
+## Contributions
+Contributions are always welcome. Please fork this repository and contribute using pull requests. The pull requests will be thoroughly assessed and if found significant will be accepted.
+
+## Lets become friends
+- [Medium](https://medium.com/@usman18)
+- [Instagram](https://www.instagram.com/usman__khan18)
+- [Twitter](https://www.twitter.com/khan_usman_18)
+- [LinkedIn](https://www.linkedin.com/in/usman-khan-7b04b1138)
+- [Github](https://github.com/usman18)
+
+My email : uk32971@gmail.com
---
+++ lib/posts_provider.dart
@@ -0,0 +1,56 @@
+
+import 'model/post.dart';
+
+class PostsProvider {
+  static final List<Post> posts = [
+    Post(username: "jayshetty", likes: "167", description: "This is some dummy text. I am trying to make it as long as possible. It is now as long as I want it to be :)",
+      imageUrl: "https://i.pinimg.com/originals/0b/68/ed/0b68ed50d769ecb55709574282477fbe.jpg",
+      profilePicUrl: "https://p1.pxfuel.com/preview/425/990/497/person-girl-kiss-child-childish-future.jpg"
+    ),
+    Post(username: "the.holistic.psychologist", likes: "222", description: "This is some dummy text. I am trying to make it as long as possible. It is now as long as I want it to be :)",
+      imageUrl: "https://media.glamour.com/photos/569579a05fff94d44eebf3ad/master/w_400%2Cc_limit/beauty-2012-01-0103-03-guy-pretty-girl-pretty-makeup-mascara_li.jpg",
+      profilePicUrl: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwyNDA3NjI4fHxlbnwwfHx8fA%3D%3D&w=1000&q=80",
+    ),
+    Post(username: "the.holistic.psychologist", likes: "562", description: "This is some dummy text. I am trying to make it as long as possible. It is now as long as I want it to be :)",
+      imageUrl: "https://media.glamour.com/photos/569579a05fff94d44eebf3ad/master/w_400%2Cc_limit/beauty-2012-01-0103-03-guy-pretty-girl-pretty-makeup-mascara_li.jpg",
+      profilePicUrl: "https://images.unsplash.com/photo-1605405747924-2709f6783d0a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwxNDQwNjd8fGVufDB8fHx8&w=1000&q=80",
+    ),
+    Post(username: "jayshetty", likes: "167", description: "This is some dummy text. I am trying to make it as long as possible. It is now as long as I want it to be :)",
+    imageUrl: "https://i.pinimg.com/originals/0b/68/ed/0b68ed50d769ecb55709574282477fbe.jpg",
+    profilePicUrl: "https://p1.pxfuel.com/preview/425/990/497/person-girl-kiss-child-childish-future.jpg"
+    ),
+    Post(username: "the.holistic.psychologist", likes: "227", description: "This is some dummy text. I am trying to make it as long as possible. It is now as long as I want it to be :)",
+      imageUrl: "https://media.glamour.com/photos/569579a05fff94d44eebf3ad/master/w_400%2Cc_limit/beauty-2012-01-0103-03-guy-pretty-girl-pretty-makeup-mascara_li.jpg",
+      profilePicUrl: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwyNDA3NjI4fHxlbnwwfHx8fA%3D%3D&w=1000&q=80",
+    ),
+    Post(username: "the.holistic.psychologist", likes: "2782", description: "This is some dummy text. I am trying to make it as long as possible. It is now as long as I want it to be :)",
+      imageUrl: "https://media.glamour.com/photos/569579a05fff94d44eebf3ad/master/w_400%2Cc_limit/beauty-2012-01-0103-03-guy-pretty-girl-pretty-makeup-mascara_li.jpg",
+      profilePicUrl: "https://images.unsplash.com/photo-1605405747924-2709f6783d0a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwxNDQwNjd8fGVufDB8fHx8&w=1000&q=80",
+    ),
+    Post(username: "jayshetty", likes: "167", description: "This is some dummy text. I am trying to make it as long as possible. It is now as long as I want it to be :)",
+    imageUrl: "https://i.pinimg.com/originals/0b/68/ed/0b68ed50d769ecb55709574282477fbe.jpg",
+    profilePicUrl: "https://p1.pxfuel.com/preview/425/990/497/person-girl-kiss-child-childish-future.jpg"
+    ),
+    Post(username: "the.holistic.psychologist", likes: "225", description: "This is some dummy text. I am trying to make it as long as possible. It is now as long as I want it to be :)",
+      imageUrl: "https://media.glamour.com/photos/569579a05fff94d44eebf3ad/master/w_400%2Cc_limit/beauty-2012-01-0103-03-guy-pretty-girl-pretty-makeup-mascara_li.jpg",
+      profilePicUrl: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwyNDA3NjI4fHxlbnwwfHx8fA%3D%3D&w=1000&q=80",
+    ),
+    Post(username: "the.holistic.psychologist", likes: "2245", description: "This is some dummy text. I am trying to make it as long as possible. It is now as long as I want it to be :)",
+      imageUrl: "https://media.glamour.com/photos/569579a05fff94d44eebf3ad/master/w_400%2Cc_limit/beauty-2012-01-0103-03-guy-pretty-girl-pretty-makeup-mascara_li.jpg",
+      profilePicUrl: "https://images.unsplash.com/photo-1605405747924-2709f6783d0a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwxNDQwNjd8fGVufDB8fHx8&w=1000&q=80",
+    ),
+    Post(username: "jayshetty", likes: "167", description: "This is some dummy text. I am trying to make it as long as possible. It is now as long as I want it to be :)",
+    imageUrl: "https://i.pinimg.com/originals/0b/68/ed/0b68ed50d769ecb55709574282477fbe.jpg",
+    profilePicUrl: "https://p1.pxfuel.com/preview/425/990/497/person-girl-kiss-child-childish-future.jpg"
+    ),
+    Post(username: "the.holistic.psychologist", likes: "7789", description: "This is some dummy text. I am trying to make it as long as possible. It is now as long as I want it to be :)",
+      imageUrl: "https://media.glamour.com/photos/569579a05fff94d44eebf3ad/master/w_400%2Cc_limit/beauty-2012-01-0103-03-guy-pretty-girl-pretty-makeup-mascara_li.jpg",
+      profilePicUrl: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwyNDA3NjI4fHxlbnwwfHx8fA%3D%3D&w=1000&q=80",
+    ),
+    Post(username: "the.holistic.psychologist", likes: "2221", description: "This is some dummy text. I am trying to make it as long as possible. It is now as long as I want it to be :)",
+      imageUrl: "https://media.glamour.com/photos/569579a05fff94d44eebf3ad/master/w_400%2Cc_limit/beauty-2012-01-0103-03-guy-pretty-girl-pretty-makeup-mascara_li.jpg",
+      profilePicUrl: "https://images.unsplash.com/photo-1605405747924-2709f6783d0a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwxNDQwNjd8fGVufDB8fHx8&w=1000&q=80",
+    ),
+
+  ];
+}
